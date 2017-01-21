3c421adad926ee7664c0d150808a76178ef3cca3
jasminish <jasmineang@hotmail.com>
2017-01-22 00:45:16 +0800

    Fixed first time not working error

--- content.js
+++ content.js
@@ -1,33 +1,29 @@
-var cats = ['cat1.png', 'cat2.png','cat3.png','cat4.png','cat5.png','cat6.png']
+var cats = ['cat1.png', 'cat2.png', 'cat3.png', 'cat4.png', 'cat5.png', 'cat6.png']
 
-var annoyingdiv = document.getElementById("annoyingcat");
+
+var div = document.createElement("DIV");
+div.id = "annoyingcat";
+var annoyingdiv = div;
+document.body.appendChild(annoyingdiv);
+
 var rmcounter = 1;
 
 function createCat() {
-	var chosen = cats[Math.floor(Math.random() * cats.length)];
-	var imgURL = chrome.extension.getURL(chosen);
-    if (annoyingdiv == null) {
-        var div = document.createElement("DIV");
-        div.id = "annoyingcat";
-
+    var chosen = cats[Math.floor(Math.random() * cats.length)];
+    var imgURL = chrome.extension.getURL(chosen);
+    for (var i = 0; i < rmcounter; i++) {
         var img = document.createElement("IMG");
         img.src = imgURL;
-        div.appendChild(img);
-        document.body.appendChild(div);
+        annoyingdiv.appendChild(img);
 
-    } else {
-        for (var i = 0; i < rmcounter; i++) {
-            var img = document.createElement("IMG");
-            img.src = imgURL;
-            annoyingdiv.appendChild(img);
-
-            img.addEventListener("click", function() {
-                img.style.visibility = 'hidden';
-                rmcounter *= 2;
-                createCat();
-            });
-        }
+        img.addEventListener("click", function() {
+            img.style.visibility = 'hidden';
+            rmcounter *= 2;
+            createCat();
+        });
     }
+    console.log(rmcounter);
+
 }
 
 createCat();
