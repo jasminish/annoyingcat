52b3d9e71fd42a2c152408098128fb040704c01e
Caryn Heng <carynheng@182-244.priv23.nus.edu.sg>
2017-01-21 23:52:47 +0800

    many cats random array

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
 var div = document.createElement("DIV");
 div.id = "annoyingcat";
-
+var cats = ['cat1.png', 'cat2.png','cat3.png']
+var chosen = cats[Math.floor(Math.random() * cats.length)];
 
 var img = document.createElement("IMG");
-var imgURL = chrome.extension.getURL('pusheen.png');
+var imgURL = chrome.extension.getURL(chosen);
 img.src = imgURL;
 div.appendChild(img);
 document.body.appendChild(div);
