015bd3010a8a9ed24c5b17e5192835bf3387a85e
jasminish <jasmineang@hotmail.com>
2017-01-21 18:34:58 +0800

    trying #1

--- content.js
+++ content.js
@@ -1,1 +1,21 @@
-alert("Hello world");
+function CAT() {
+    var imgURL = chrome.extension.getURL("pusheen.png");
+    document.getElementByTagName('body')[0].prepend('<img id="CATimg" />');
+
+    var $catImg = document.getElementById('CATimg');
+
+    if (document.contains($catImg)) {
+        $catImg.src = imgURL;
+        $catImg.style.position = 'fixed';
+        $catImg.style.width = 25;
+        $catImg.style.height = 'auto';
+        $catImg.style.display = 'none';
+        $catImg.style.zIndex = 2147483647;
+        $catImg.style.border = '0px';
+        $catImg.style.padding = '0px';
+        $catImg.style.bottom = '20px';
+        $catImg.style.right = '20px';
+        $catImg.style.margin = '0px 0px 0px 0px';
+    }
+}
+CAT();
