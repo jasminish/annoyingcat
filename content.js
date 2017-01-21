4eb538f94c716dab2eb143c35d015d351283caa3
jasminish <jasmineang@hotmail.com>
2017-01-21 19:51:42 +0800

    fix broken image

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ div.id = "annoyingcat";
 
 
 var img = document.createElement("IMG");
-var imgURL = chrome.extension.getURL('/pusheen.png');
+var imgURL = chrome.extension.getURL('pusheen.png');
 img.src = imgURL;
 div.appendChild(img);
 document.body.appendChild(div);
