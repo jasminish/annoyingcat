ea38b2e54b2df07d9ba9b7293dcaec34edc877a3
jasminish <jasmineang@hotmail.com>
2017-01-22 02:55:09 +0800

    Randomise positions of cats

--- content.js
+++ content.js
@@ -12,11 +12,17 @@ document.body.appendChild(annoyingdiv);
 var rmcounter = 1;
 
 function createCat() {
-    var chosen = cats[Math.floor(Math.random() * cats.length)];
-    var imgURL = chrome.extension.getURL(chosen);
     for (var i = 0; i < rmcounter; i++) {
+        var chosen = cats[Math.floor(Math.random() * cats.length)];
+        var imgURL = chrome.extension.getURL(chosen);
+
+        var top = Math.floor(Math.random()*90)+5;
+        var left = Math.floor(Math.random()*90)+5;
+
         var img = document.createElement("IMG");
         img.src = imgURL;
+        img.style.top = top + '%';
+        img.style.left = left + '%';
         annoyingdiv.appendChild(img);
 
         img.addEventListener("click", function() {
