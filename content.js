1b254c578dcb0a07fad9efa8fe5f09a7475accbc
Ian <kishontcf@gmail.com>
2017-01-21 19:54:28 +0800

    background.js modified to be compatible with content.js alert in background.js disabled

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
+
 var div = document.createElement("DIV");
 div.id = "annoyingcat";
 
 
 var img = document.createElement("IMG");
-var imgURL = chrome.extension.getURL('/pusheen.png');
+var imgURL = chrome.extension.getURL("pusheen.png");
 img.src = imgURL;
 div.appendChild(img);
 document.body.appendChild(div);
