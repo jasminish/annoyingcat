73a7527db02dd9530d45097658eb1a44939aae25
Caryn Heng <carynheng@182-244.priv23.nus.edu.sg>
2017-01-22 00:03:31 +0800

    randomised cats wheee

--- content.js
+++ content.js
@@ -1,19 +1,16 @@
 var cats = ['cat1.png', 'cat2.png','cat3.png','cat4.png','cat5.png','cat6.png']
-var chosen = cats[Math.floor(Math.random() * cats.length)];
-
-
-var imgURL = chrome.extension.getURL(chosen);
 
 var annoyingdiv = document.getElementById("annoyingcat");
 var rmcounter = 1;
 
 function createCat() {
+	var chosen = cats[Math.floor(Math.random() * cats.length)];
+	var imgURL = chrome.extension.getURL(chosen);
     if (annoyingdiv == null) {
         var div = document.createElement("DIV");
         div.id = "annoyingcat";
 
         var img = document.createElement("IMG");
-        var imgURL = chrome.extension.getURL('pusheen.png');
         img.src = imgURL;
         div.appendChild(img);
         document.body.appendChild(div);
@@ -21,7 +18,6 @@ function createCat() {
     } else {
         for (var i = 0; i < rmcounter; i++) {
             var img = document.createElement("IMG");
-            var imgURL = chrome.extension.getURL('pusheen.png');
             img.src = imgURL;
             annoyingdiv.appendChild(img);
 
