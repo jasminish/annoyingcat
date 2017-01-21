d635804df402636bd7fe738b3aea61ba2fb2e37a
jasminish <jasmineang@hotmail.com>
2017-01-21 19:16:52 +0800

    display cat

--- content.js
+++ content.js
@@ -1,21 +1,9 @@
-function CAT() {
-    var imgURL = chrome.extension.getURL("pusheen.png");
-    document.getElementByTagName('body')[0].prepend('<img id="CATimg" />');
+var div = document.createElement("DIV");
+div.id = "annoyingcat";
 
-    var $catImg = document.getElementById('CATimg');
 
-    if (document.contains($catImg)) {
-        $catImg.src = imgURL;
-        $catImg.style.position = 'fixed';
-        $catImg.style.width = 25;
-        $catImg.style.height = 'auto';
-        $catImg.style.display = 'none';
-        $catImg.style.zIndex = 2147483647;
-        $catImg.style.border = '0px';
-        $catImg.style.padding = '0px';
-        $catImg.style.bottom = '20px';
-        $catImg.style.right = '20px';
-        $catImg.style.margin = '0px 0px 0px 0px';
-    }
-}
-CAT();
+var img = document.createElement("IMG");
+var imgURL = chrome.extension.getURL('/pusheen.png');
+img.src = imgURL;
+div.appendChild(img);
+document.body.appendChild(div);
